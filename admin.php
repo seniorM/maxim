2de640190a293359cb9fa2b5db73dae45b7b248b
user <Global@mail.ua>
2019-08-30 21:02:57 +0300

    add folders

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ else
 {
 echo('
 <form method="post">
-Логин: <input type="text" name=»login» />
+Логин: <input type="text" name="login" />
 Пароль: <input type="password" name="password" />
 <input type="submit" value="Войти" name="btn_auth" />
 </form>
---
+++ adminEdit.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+		<title>Админ панель</title>
+	</head>
+		<body><h1>Админ панель</h1>
+		    <p><a href="editGallery.php">Редактирование галлереи</a> | <a href="editeMain.php">Редактирование главной</a></p>
+<hr />
+
+		</body>
+</html>
+
